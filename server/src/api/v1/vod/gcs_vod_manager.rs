0178feabaa81e4fb4320f2edd87b80b818eaf7dc
Michael Bao <mike.h.bao@gmail.com>
2020-12-21 20:43:08 -0500

    fix large memory usage of vod fastify

--- server/src/api/v1/vod/gcs_vod_manager.rs
+++ server/src/api/v1/vod/gcs_vod_manager.rs
@@ -3,10 +3,16 @@ use squadov_common;
 use std::sync::Arc;
 use std::collections::BTreeMap;
 use std::io::Write;
+use std::fs::File;
+use std::io::Read;
 
 use async_trait::async_trait;
+use rand::Rng;
+use actix_web::web::{BytesMut, Bytes};
 
 const GS_URI_PREFIX : &'static str = "gs://";
+const MAX_GCS_RETRIES: i32 = 10;
+const MAX_GCS_BACKOFF_TIME_MS: i64 = 32000;
 
 pub struct GCSVodManager {
     bucket: String,
@@ -70,10 +76,48 @@ impl v1::VodManager for GCSVodManager {
     async fn upload_vod_from_file(&self, segment: &squadov_common::VodSegmentId, path: &std::path::Path) -> Result<(), squadov_common::SquadOvError> {
         let client = self.get_gcp_client().gcs();
         let fname = self.get_path_parts_from_segment_id(segment);
-        // Maybe better to do some kind of streaming upload instead so we don't
-        // have to have the entire VOD in memory at the same time.
-        let vod_data = std::fs::read(path)?;
-        client.upload_object(&self.bucket, &fname, &vod_data).await?;
+        
+        // Stream from the file to GCS.
+        let mut f = File::open(path)?;
+        let mut buffer = BytesMut::with_capacity(8 * 1024 * 1024);
+        buffer.resize(8 * 1024 * 1024, 0);
+
+        log::info!("Start resumable upload session: {:?}", segment);
+        let session = client.initiate_resumable_upload_session(&self.bucket, &fname).await?;
+        log::info!("Obtained resumable upload session: {:?} :: {}", segment, session);
+        let mut total_bytes: usize = 0;
+        loop {
+            let n = f.read(&mut buffer)?;
+            let last = n < buffer.len();
+            let mut success = false;
+            for i in 0..MAX_GCS_RETRIES {
+                let byte_buffer = Bytes::copy_from_slice(&buffer[0..n]);
+                match client.upload_resumable_object(&session, total_bytes, byte_buffer, last).await {
+                    Ok(_) => {
+                        success = true;
+                        break;
+                    },
+                    Err(err) => {
+                        let backoff_ms = {
+                            let mut rng = rand::thread_rng();
+                            std::cmp::min(2i64.pow(i as u32) +  rng.gen_range(0..1000), MAX_GCS_BACKOFF_TIME_MS)
+                        };
+                        log::warn!("Failed to upload to GCS: {:?} - Retrying {} @{}ms", err, i, backoff_ms);
+                        async_std::task::sleep(std::time::Duration::from_millis(backoff_ms as u64)).await;
+                    }
+                }
+            }
+
+            if !success {
+                return Err(squadov_common::SquadOvError::InternalError(String::from("Max GCS upload retry limit exceeded.")));
+            }
+            
+            total_bytes += n;
+            if last {
+                break;
+            }
+        }
+
         Ok(())
     }
 
