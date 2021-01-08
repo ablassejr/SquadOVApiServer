1beccbd5bbfaf6be450f0156bf37c72880207e36
Michael Bao <mike.h.bao@gmail.com>
2021-01-08 14:36:31 -0500

    increase speed of WoW stat query by 3-5x by using an index on combat log unit ownership and not querying data within intervals

---
+++ devops/database/sql/V0020.3__CombatLogIndex.sql
@@ -0,0 +1,1 @@
+CREATE INDEX ON wow_combatlog_unit_ownership (unit_guid);
--- server/src/api/v1/wow/stats.rs
+++ server/src/api/v1/wow/stats.rs
@@ -8,16 +8,12 @@ use serde::{Serialize, Deserialize};
 use uuid::Uuid;
 use chrono::{DateTime,Utc};
 use std::collections::HashMap;
-use sqlx::postgres::types::PgInterval;
 
 #[derive(Deserialize)]
 pub struct WowStatsQueryParams {
     // How often we sample the start - end time range.
     #[serde(rename="psStepSeconds")]
     ps_step_seconds: i64,
-    // For each sample, the time (half) range to perform an average over.
-    #[serde(rename="psIntervalSeconds")]
-    ps_interval_seconds: i64,
     #[serde(deserialize_with="squadov_common::parse_utc_time_from_milliseconds")]
     start: Option<DateTime<Utc>>,
     #[serde(deserialize_with="squadov_common::parse_utc_time_from_milliseconds")]
@@ -36,21 +32,14 @@ impl api::ApiApplication {
             return Err(SquadOvError::BadRequest);
         }
 
-        let step_interval = PgInterval{
-            months: 0,
-            days: 0,
-            microseconds: params.ps_step_seconds * (1e+6 as i64),
-        };
-
-        let sample_interval = PgInterval{
-            months: 0,
-            days: 0,
-            microseconds: params.ps_interval_seconds * (1e+6 as i64),
-        };
-
         let raw_stats = sqlx::query!(
             r#"
-            WITH match_combat_logs AS (
+            SELECT
+                FLOOR((EXTRACT(EPOCH FROM mcl.tm) - EXTRACT(EPOCH FROM $3)) / $5::BIGINT) * $5::BIGINT AS "xtm",
+                COALESCE(mcl.owner_guid, mcl.source->>'guid') AS "guid",
+                SUM((mcl.evt->>'amount')::INTEGER) / CAST($5::BIGINT AS DOUBLE PRECISION) AS "amount"
+            FROM squadov.wow_match_combatants AS wmc
+            LEFT JOIN LATERAL (
                 SELECT wce.*, wuco.owner_guid
                 FROM squadov.wow_combat_log_events AS wce
                 INNER JOIN squadov.wow_combat_logs AS wcl
@@ -58,25 +47,15 @@ impl api::ApiApplication {
                 INNER JOIN squadov.wow_match_combat_log_association AS wma
                     ON wma.combat_log_uuid = wcl.uuid
                 LEFT JOIN squadov.wow_combatlog_unit_ownership AS wuco
-                    ON wuco.unit_guid = wce.source->>'guid'
+                    ON wuco.combat_log_uuid = wcl.uuid
+                        AND wuco.unit_guid = wce.source->>'guid'
                 WHERE wcl.user_id = $1
                     AND wma.match_uuid = $2
-            )
-            SELECT
-                generate_series AS "xtm",
-                COALESCE(mcl.owner_guid, mcl.source->>'guid') AS "guid",
-                SUM((mcl.evt->>'amount')::INTEGER) / CAST($7::BIGINT AS DOUBLE PRECISION) AS "amount"
-            FROM generate_series(
-                $3::TIMESTAMPTZ,
-                $4::TIMESTAMPTZ,
-                $5::INTERVAL
-            )
-            CROSS JOIN squadov.wow_match_combatants AS wmc
-            LEFT JOIN match_combat_logs AS mcl
-                ON mcl.tm BETWEEN (generate_series - $6::INTERVAL) AND (generate_series + $6::INTERVAL)
-                    AND (mcl.source->>'guid' = wmc.combatant_guid OR mcl.owner_guid = wmc.combatant_guid)
-            WHERE mcl.evt @> '{"type": "DamageDone"}'
-                AND wmc.match_uuid = $2
+                    AND wce.tm BETWEEN $3 AND $4
+                    AND wce.evt @> '{"type": "DamageDone"}'
+                    AND (wce.source->>'guid' = wmc.combatant_guid OR wuco.owner_guid = wmc.combatant_guid)
+            ) AS mcl ON TRUE
+            WHERE wmc.match_uuid = $2
             GROUP BY xtm, guid
             ORDER BY xtm, guid
             "#,
@@ -84,9 +63,7 @@ impl api::ApiApplication {
             match_uuid,
             params.start.unwrap(),
             params.end.unwrap(),
-            step_interval,
-            sample_interval,
-            params.ps_interval_seconds * 2
+            params.ps_step_seconds,
         )
             .fetch_all(&*self.pool)
             .await?;
@@ -107,7 +84,7 @@ impl api::ApiApplication {
 
             let inner_vec = ret_map.get_mut(guid).unwrap();
             inner_vec.push(WowStatDatum{
-                tm: (*tm - params.start.unwrap()).num_milliseconds() as f64 / 1000.0,
+                tm: *tm,
                 value: *amount,
             });
         }
@@ -119,21 +96,14 @@ impl api::ApiApplication {
             return Err(SquadOvError::BadRequest);
         }
 
-        let step_interval = PgInterval{
-            months: 0,
-            days: 0,
-            microseconds: params.ps_step_seconds * (1e+6 as i64),
-        };
-
-        let sample_interval = PgInterval{
-            months: 0,
-            days: 0,
-            microseconds: params.ps_interval_seconds * (1e+6 as i64),
-        };
-
         let raw_stats = sqlx::query!(
             r#"
-            WITH match_combat_logs AS (
+            SELECT
+                FLOOR((EXTRACT(EPOCH FROM mcl.tm) - EXTRACT(EPOCH FROM $3)) / $5::BIGINT) * $5::BIGINT AS "xtm",
+                mcl.source->>'guid' AS "guid",
+                SUM(GREATEST((mcl.evt->>'amount')::INTEGER - (mcl.evt->>'overheal')::INTEGER, 0)) / CAST($5::BIGINT AS DOUBLE PRECISION) AS "amount"
+            FROM squadov.wow_match_combatants AS wmc
+            LEFT JOIN LATERAL (
                 SELECT wce.*
                 FROM squadov.wow_combat_log_events AS wce
                 INNER JOIN squadov.wow_combat_logs AS wcl
@@ -142,22 +112,11 @@ impl api::ApiApplication {
                     ON wma.combat_log_uuid = wcl.uuid
                 WHERE wcl.user_id = $1
                     AND wma.match_uuid = $2
-            )
-            SELECT
-                generate_series AS "xtm",
-                mcl.source->>'guid' AS "guid",
-                SUM(GREATEST((mcl.evt->>'amount')::INTEGER - (mcl.evt->>'overheal')::INTEGER, 0)) / CAST($7::BIGINT AS DOUBLE PRECISION) AS "amount"
-            FROM generate_series(
-                $3::TIMESTAMPTZ,
-                $4::TIMESTAMPTZ,
-                $5::INTERVAL
-            )
-            CROSS JOIN squadov.wow_match_combatants AS wmc
-            LEFT JOIN match_combat_logs AS mcl
-                ON mcl.tm BETWEEN (generate_series - $6::INTERVAL) AND (generate_series + $6::INTERVAL)
-                    AND mcl.source->>'guid' = wmc.combatant_guid
-            WHERE mcl.evt @> '{"type": "Healing"}'
-                AND wmc.match_uuid = $2
+                    AND wce.tm BETWEEN $3 AND $4
+                    AND wce.evt @> '{"type": "Healing"}'
+                    AND wce.source->>'guid' = wmc.combatant_guid
+            ) AS mcl ON TRUE
+            WHERE wmc.match_uuid = $2
             GROUP BY xtm, guid
             ORDER BY xtm, guid
             "#,
@@ -165,9 +124,7 @@ impl api::ApiApplication {
             match_uuid,
             params.start.unwrap(),
             params.end.unwrap(),
-            step_interval,
-            sample_interval,
-            params.ps_interval_seconds * 2
+            params.ps_step_seconds,
         )
             .fetch_all(&*self.pool)
             .await?;
@@ -188,7 +145,7 @@ impl api::ApiApplication {
 
             let inner_vec = ret_map.get_mut(guid).unwrap();
             inner_vec.push(WowStatDatum{
-                tm: (*tm - params.start.unwrap()).num_milliseconds() as f64 / 1000.0,
+                tm: *tm,
                 value: *amount,
             });
         }
@@ -196,26 +153,18 @@ impl api::ApiApplication {
     }
 
     async fn get_wow_match_damage_received_per_second(&self, user_id: i64, match_uuid: &Uuid, params: &WowStatsQueryParams) -> Result<HashMap<String, Vec<WowStatDatum>>, SquadOvError> {
-
         if params.start.is_none() || params.end.is_none() {
             return Err(SquadOvError::BadRequest);
         }
 
-        let step_interval = PgInterval{
-            months: 0,
-            days: 0,
-            microseconds: params.ps_step_seconds * (1e+6 as i64),
-        };
-
-        let sample_interval = PgInterval{
-            months: 0,
-            days: 0,
-            microseconds: params.ps_interval_seconds * (1e+6 as i64),
-        };
-
         let raw_stats = sqlx::query!(
             r#"
-            WITH match_combat_logs AS (
+            SELECT
+                FLOOR((EXTRACT(EPOCH FROM mcl.tm) - EXTRACT(EPOCH FROM $3)) / $5::BIGINT) * $5::BIGINT AS "xtm",
+                mcl.dest->>'guid' AS "guid",
+                SUM((mcl.evt->>'amount')::INTEGER) / CAST($5::BIGINT AS DOUBLE PRECISION) AS "amount"
+            FROM squadov.wow_match_combatants AS wmc
+            LEFT JOIN LATERAL (
                 SELECT wce.*
                 FROM squadov.wow_combat_log_events AS wce
                 INNER JOIN squadov.wow_combat_logs AS wcl
@@ -224,22 +173,11 @@ impl api::ApiApplication {
                     ON wma.combat_log_uuid = wcl.uuid
                 WHERE wcl.user_id = $1
                     AND wma.match_uuid = $2
-            )
-            SELECT
-                generate_series AS "xtm",
-                mcl.dest->>'guid' AS "guid",
-                SUM((mcl.evt->>'amount')::INTEGER) / CAST($7::BIGINT AS DOUBLE PRECISION) AS "amount"
-            FROM generate_series(
-                $3::TIMESTAMPTZ,
-                $4::TIMESTAMPTZ,
-                $5::INTERVAL
-            )
-            CROSS JOIN squadov.wow_match_combatants AS wmc
-            LEFT JOIN match_combat_logs AS mcl
-                ON mcl.tm BETWEEN (generate_series - $6::INTERVAL) AND (generate_series + $6::INTERVAL)
-                    AND mcl.dest->>'guid' = wmc.combatant_guid
-            WHERE mcl.evt @> '{"type": "DamageDone"}'
-                AND wmc.match_uuid = $2
+                    AND wce.tm BETWEEN $3 AND $4
+                    AND wce.evt @> '{"type": "DamageDone"}'
+                    AND wce.dest->>'guid' = wmc.combatant_guid
+            ) AS mcl ON TRUE
+            WHERE wmc.match_uuid = $2
             GROUP BY xtm, guid
             ORDER BY xtm, guid
             "#,
@@ -247,9 +185,7 @@ impl api::ApiApplication {
             match_uuid,
             params.start.unwrap(),
             params.end.unwrap(),
-            step_interval,
-            sample_interval,
-            params.ps_interval_seconds * 2
+            params.ps_step_seconds,
         )
             .fetch_all(&*self.pool)
             .await?;
@@ -270,7 +206,7 @@ impl api::ApiApplication {
 
             let inner_vec = ret_map.get_mut(guid).unwrap();
             inner_vec.push(WowStatDatum{
-                tm: (*tm - params.start.unwrap()).num_milliseconds() as f64 / 1000.0,
+                tm: *tm,
                 value: *amount,
             });
         }
