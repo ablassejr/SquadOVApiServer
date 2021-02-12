e683c859b844098a0d3c79ac648d974727c43b9c
Michael Bao <mike.h.bao@gmail.com>
2021-02-12 15:42:45 -0500

    v0.5.12 stuff

---
+++ news/data/hotfix-v0.5.12.md
@@ -0,0 +1,5 @@
+Hello /r/wow and /r/worldofpvp! Enjoy your stay! (:
+
+## Bug Fixes
+* Fixed a crash due to having more than 2 audio channels (5.1/7.1 setups).
+* Fixed an issue where SquadOV wouldn't detect some fullscreen windowed apps as being fullscreen due to the resolution being a pixel off.
--- server/src/api/v1/vod/delete.rs
+++ server/src/api/v1/vod/delete.rs
@@ -10,35 +10,52 @@ pub struct VodDeleteFromUuid {
     video_uuid: Uuid,
 }
 
+impl api::ApiApplication {
+
+    async fn delete_vod(&self, video_uuid: &Uuid) -> Result<(), SquadOvError> {
+        sqlx::query!(
+            "
+            DELETE FROM squadov.vods
+            WHERE video_uuid = $1
+            ",
+            video_uuid
+        )
+            .execute(&*self.pool)
+            .await?;
+        Ok(())
+    }
+
+}
+
 pub async fn delete_vod_handler(data : web::Path<VodDeleteFromUuid>, app : web::Data<Arc<api::ApiApplication>>) -> Result<HttpResponse, SquadOvError> {
     let quality_options = app.get_vod_quality_options(&[data.video_uuid.clone()]).await?;
     let quality_arr = quality_options.get(&data.video_uuid).ok_or(SquadOvError::NotFound)?;
     let vod = app.find_vod_associations(&[data.video_uuid.clone()]).await?.get(&data.video_uuid).ok_or(SquadOvError::NotFound)?.clone();
     let raw_extension = squadov_common::container_format_to_extension(&vod.raw_container_format);
-
+    app.delete_vod(&data.video_uuid).await?;
     for quality in quality_arr {
-        app.vod.delete_vod(&squadov_common::VodSegmentId{
+        let _ = app.vod.delete_vod(&squadov_common::VodSegmentId{
             video_uuid: data.video_uuid.clone(),
             quality: quality.id.clone(),
             segment_name: format!("video.{}", &raw_extension),
-        }).await?;
+        }).await;
 
         if quality.has_fastify {
-            app.vod.delete_vod(&squadov_common::VodSegmentId{
+            let _ = app.vod.delete_vod(&squadov_common::VodSegmentId{
                 video_uuid: data.video_uuid.clone(),
                 quality: quality.id.clone(),
                 segment_name: String::from("fastify.mp4"),
-            }).await?;
+            }).await;
         }
 
         if quality.has_preview {
-            app.vod.delete_vod(&squadov_common::VodSegmentId{
+            let _ = app.vod.delete_vod(&squadov_common::VodSegmentId{
                 video_uuid: data.video_uuid.clone(),
                 quality: quality.id.clone(),
                 segment_name: String::from("preview.mp4"),
-            }).await?;
+            }).await;
         }
     }
 
-    return Ok(HttpResponse::Ok().finish());
+    Ok(HttpResponse::Ok().finish())
 }
