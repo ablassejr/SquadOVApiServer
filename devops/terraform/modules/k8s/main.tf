91bd22590b06258d68224d594c50c387ce0b69af
Michael Bao <mike.h.bao@gmail.com>
2021-02-23 15:44:26 -0500

    k8s deployment of support page

--- devops/terraform/modules/k8s/main.tf
+++ devops/terraform/modules/k8s/main.tf
@@ -6,6 +6,10 @@ resource "google_container_cluster" "primary" {
     initial_node_count = 1
 }
 
+resource "google_compute_global_address" "squadov-static-ip" {
+    name = "squadov-static-ip"
+}
+
 resource "google_container_node_pool" "primary_wow_worker_nodes" {
     name = "squadov-primary-wow-worker-nodes"
     location = "us-central1-c"
@@ -57,5 +61,32 @@ resource "google_container_node_pool" "vod_worker_nodes" {
     autoscaling {
         min_node_count = 1
         max_node_count = 8
+    }
+}
+
+resource "google_container_node_pool" "static_nodes" {
+    name = "squadov-primary-static-nodes"
+    location = "us-central1-c"
+    cluster = google_container_cluster.primary.name
+    initial_node_count =  1
+    lifecycle {
+        ignore_changes = [
+            initial_node_count
+        ]
+    }
+    
+    node_config {
+        labels = {
+            task = "static"
+        }
+        disk_size_gb = 20
+        disk_type = "pd-ssd"
+        machine_type = "e2-custom-2-4096"
+        image_type = "COS"
+    }
+
+    autoscaling {
+        min_node_count = 1
+        max_node_count = 1
     }
 }
