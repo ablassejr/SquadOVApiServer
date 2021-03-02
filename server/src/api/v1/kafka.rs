afb261e912dcac78a6b7c16cc01201179fed9cb5
Michael Bao <mike.h.bao@gmail.com>
2021-03-02 16:24:33 -0500

    send wow topic info via kafka endpoint

--- server/src/api/v1/kafka.rs
+++ server/src/api/v1/kafka.rs
@@ -5,10 +5,12 @@ use std::sync::Arc;
 use serde::Serialize;
 
 #[derive(Serialize)]
+#[serde(rename_all="camelCase")]
 struct KafkaInfo {
     servers: String,
     key: String,
-    secret: String
+    secret: String,
+    wow_topic: String,
 }
 
 pub async fn get_kafka_info_handler(app : web::Data<Arc<api::ApiApplication>>) -> Result<HttpResponse, SquadOvError> {
@@ -16,5 +18,6 @@ pub async fn get_kafka_info_handler(app : web::Data<Arc<api::ApiApplication>>) -
         servers: app.config.kafka.bootstrap_servers.clone(),
         key: app.config.kafka.client_keypair.key.clone(),
         secret: app.config.kafka.client_keypair.secret.clone(),
+        wow_topic: app.config.kafka.wow_combat_log_topic.clone(),
     }))
 }
