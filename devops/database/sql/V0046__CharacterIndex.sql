150443bab183bfc097e3046b32c55bb1d0bec431
Michael Bao <mike.h.bao@gmail.com>
2021-03-30 17:57:08 -0400

    add index to character id for the full WoW character info queries
    fixes #397

---
+++ devops/database/sql/V0046__CharacterIndex.sql
@@ -0,0 +1,3 @@
+CREATE INDEX ON wow_match_view_combatant_items (character_id);
+CREATE INDEX ON wow_match_view_combatant_talents(character_id);
+CREATE INDEX ON wow_match_view_combatant_covenants (character_id);
