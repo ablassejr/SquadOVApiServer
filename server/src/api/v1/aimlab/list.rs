3eefe3199130a102baa85e44fd6b4e8c528d3f56
Michael Bao <mike.h.bao@gmail.com>
2021-05-26 14:26:39 -0400

    aimlab task filters
    for https://gitlab.com/squadov/SquadOVClient/-/issues/628

--- server/src/api/v1/aimlab/list.rs
+++ server/src/api/v1/aimlab/list.rs
@@ -7,26 +7,43 @@ use actix_web::{web, HttpResponse, HttpRequest};
 use serde::Deserialize;
 use std::sync::Arc;
 use uuid::Uuid;
+use serde_qs::actix::QsQuery;
 
 #[derive(Deserialize)]
 pub struct AimlabUserTaskListInput {
     user_id: i64,
 }
 
+#[derive(Deserialize, Debug)]
+#[serde(rename_all="camelCase")]
+pub struct AimlabListQuery {
+    tasks: Option<Vec<String>>,
+    has_vod: Option<bool>,
+}
+
 impl api::ApiApplication {
-    pub async fn list_aimlab_matches_for_user(&self, user_id: i64, start: i64, end: i64) -> Result<Vec<AimlabTask>, SquadOvError> {
+    pub async fn list_aimlab_matches_for_user(&self, user_id: i64, start: i64, end: i64, filter: &AimlabListQuery) -> Result<Vec<AimlabTask>, SquadOvError> {
         let matches = sqlx::query_as!(
             AimlabTask,
             "
-            SELECT *
-            FROM squadov.aimlab_tasks
-            WHERE user_id = $1
-            ORDER BY create_date DESC
+            SELECT at.*
+            FROM squadov.aimlab_tasks AS at
+            INNER JOIN squadov.users AS u
+                ON u.id = at.user_id
+            LEFT JOIN squadov.vods AS v
+                ON v.match_uuid = at.match_uuid
+                    AND v.user_uuid = u.uuid
+            WHERE at.user_id = $1
+                AND (CARDINALITY($4::VARCHAR[]) = 0 OR at.task_name = ANY($4))
+                AND (NOT $5::BOOLEAN OR v.video_uuid IS NOT NULL)
+            ORDER BY at.create_date DESC
             LIMIT $2 OFFSET $3
             ",
             user_id,
             end - start,
-            start
+            start,
+            &filter.tasks.as_ref().unwrap_or(&vec![]).iter().map(|x| { x.clone() }).collect::<Vec<String>>(),
+            filter.has_vod.unwrap_or(false),
         )
             .fetch_all(&*self.pool)
             .await?;
@@ -50,12 +67,13 @@ impl api::ApiApplication {
     }
 }
 
-pub async fn list_aimlab_matches_for_user_handler(data : web::Path<AimlabUserTaskListInput>, query: web::Query<api::PaginationParameters>, app : web::Data<Arc<api::ApiApplication>>, req: HttpRequest) -> Result<HttpResponse, squadov_common::SquadOvError> {
+pub async fn list_aimlab_matches_for_user_handler(data : web::Path<AimlabUserTaskListInput>, query: web::Query<api::PaginationParameters>, filter: QsQuery<AimlabListQuery>, app : web::Data<Arc<api::ApiApplication>>, req: HttpRequest) -> Result<HttpResponse, squadov_common::SquadOvError> {
     let query = query.into_inner();
     let matches = app.list_aimlab_matches_for_user(
         data.user_id,
         query.start,
         query.end,
+        &filter,
     ).await?;
 
     let expected_total = query.end - query.start;
