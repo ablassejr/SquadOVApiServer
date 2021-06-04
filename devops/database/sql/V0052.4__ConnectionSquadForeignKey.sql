a25c7801d739790b03b0af64ecf23f35ac5fc849
Michael Bao <mike.h.bao@gmail.com>
2021-06-04 19:00:19 -0400

    remove share connections that share to a squad that a user is no longer a part of
    fixes https://gitlab.com/squadov/SquadOVClient/-/issues/697

--- devops/database/sql/V0052.4__ConnectionSquadForeignKey.sql
+++ devops/database/sql/V0052.4__ConnectionSquadForeignKey.sql
@@ -1,11 +1,49 @@
-DELETE FROM share_match_vod_connections AS mvc
-WHERE NOT EXISTS (
-    SELECT *
-    FROM squad_role_assignments AS sra
-    WHERE sra.squad_id = mvc.dest_squad_id
+CREATE TABLE squadov.new_share_match_vod_connections AS 
+SELECT mvc.*
+FROM squadov.share_match_vod_connections AS mvc
+LEFT JOIN squadov.squad_role_assignments AS sra
+    ON sra.squad_id = mvc.dest_squad_id
         AND sra.user_id = mvc.source_user_id
-) AND mvc.dest_squad_id IS NOT NULL;
+WHERE sra.squad_id IS NOT NULL
+    AND mvc.dest_squad_id IS NOT NULL
+UNION
+SELECT mvc.*
+FROM squadov.share_match_vod_connections AS mvc
+WHERE mvc.dest_squad_id IS NULL;
 
-ALTER TABLE share_match_vod_connections
-ADD CONSTRAINT share_match_vod_connections_dest_squad_id_source_user_id_fkey
+ALTER TABLE squadov.new_share_match_vod_connections
+ADD CONSTRAINT new_share_match_vod_connections_dest_squad_id_source_user_id_fk
 FOREIGN KEY (dest_squad_id, source_user_id) REFERENCES squad_role_assignments(squad_id, user_id) ON DELETE CASCADE;
+
+CREATE OR REPLACE VIEW squadov.view_share_connections_access_users (
+    id,
+    match_uuid,
+    video_uuid,
+    user_id,
+    parent_connection_id,
+    source_user_id
+)
+AS
+SELECT
+    f.id,
+    f.match_uuid,
+    f.video_uuid,
+    f.user_id,
+    f.parent_connection_id,
+    f.source_user_id
+FROM (
+    SELECT mvc.id, mvc.match_uuid, mvc.video_uuid, mvc.dest_user_id, mvc.share_depth, mvc.parent_connection_id, mvc.source_user_id
+    FROM squadov.new_share_match_vod_connections AS mvc
+    WHERE mvc.dest_user_id IS NOT NULL
+    UNION
+    SELECT mvc.id, mvc.match_uuid, mvc.video_uuid, sra.user_id, mvc.share_depth, mvc.parent_connection_id, mvc.source_user_id
+    FROM squadov.new_share_match_vod_connections AS mvc
+    INNER JOIN squadov.squad_role_assignments AS sra
+        ON sra.squad_id = mvc.dest_squad_id
+    WHERE mvc.dest_squad_id IS NOT NULL
+        AND sra.user_id != mvc.source_user_id
+) AS f(id, match_uuid, video_uuid, user_id, share_depth, parent_connection_id, source_user_id)
+ORDER BY f.match_uuid, f.video_uuid, f.user_id, f.share_depth ASC;
+
+DROP TABLE squadov.share_match_vod_connections CASCADE;
+ALTER TABLE squadov.new_share_match_vod_connections RENAME TO share_match_vod_connections;
