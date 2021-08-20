9a7d533bf05ae7438dfc61efdf1e6b8e4fb48808
Michael Bao <mike@grchive.com>
2021-08-19 23:43:31 -0400

    add Cloudfront distributions for public and private viewing of VODs stored on AWS
    for #1164

--- devops/terraform-aws/modules/storage/main.tf
+++ devops/terraform-aws/modules/storage/main.tf
@@ -1,19 +1,10 @@
-
-/*
-resource "aws_s3_bucket" "log_storage_bucket" {
-    bucket = "squadov-us-log-bucket${var.bucket_suffix}"
-    acl = "private"
-    tags = {
-        "s3" = "logs"
-    }
-}
-
-resource "aws_cloudfront_origin_access_identity" "private_vod_access_identity" {    
+resource "aws_cloudfront_origin_access_identity" "private_vod_access_identity" {
+    comment = "For Signed URL access to VODs"
 }
 
 resource "aws_cloudfront_origin_access_identity" "public_vod_access_identity" {    
+    comment = "For public access to VODs"
 }
-*/
 
 resource "aws_s3_bucket" "vod_storage_bucket" {
     bucket = "squadov-us-vod-bucket${var.bucket_suffix}"
@@ -38,64 +29,70 @@ resource "aws_s3_bucket" "vod_storage_bucket" {
 
     tags = {
         "s3" = "vods"
+        "vods" = "raw_s3"
     }
 }
 
-/*
-locals {
-    private_s3_origin_id = "privateVodS3Origin"
-}
+data "aws_iam_policy_document" "vod_access_policy" {
+    statement {
+        actions = ["s3:GetObject"]
+        effect = "Allow"
 
-resource "aws_cloudfront_distribution" "private_vod_distribution" {
-    default_cache_behavior {
-        allowed_methods = [ "OPTIONS", "HEAD", "GET" ]
-        cached_methods = [ "GET", "HEAD" ]
-        target_origin_id = local.private_s3_origin_id
-
-        min_ttl = 600
-        default_ttl = 3600
-        max_ttl = 86400
-
-        forwarded_values {
-            query_string = true
-            headers = "*"
-            cookies {
-                forward = "none"
-            }
+        condition {
+            test = "StringEquals"
+            values = ["public"]
+            variable = "s3:ExistingObjectTag/access"
         }
 
-        viewer_protocol_policy = "redirect-to-https"
-    }
-
-    enabled = true
-    is_ipv6_enabled = true
-
-    logging_config {
-        bucket = aws_s3_bucket.log_storage_bucket.bucket_domain_name
-        include_cookies = false
-        prefix = "vods_private/"
-    }
-
-    origin {
-        domain_name = aws_s3_bucket.vod_storage_bucket.bucket_domain_name
-        origin_id = local.private_s3_origin_id
-
-        s3_origin_config {
-            origin_access_identity = aws_cloudfront_origin_access_identity.private_vod_access_identity.cloudfront_access_identity_path
+        principals {
+            type = "AWS"
+            identifiers = [aws_cloudfront_origin_access_identity.public_vod_access_identity.iam_arn]
         }
+
+        resources = [ "${aws_s3_bucket.vod_storage_bucket.arn}/*"]
+
+        sid = "cloudfront-public-access"
     }
 
-    price_class = "PriceClass_All"
+    statement {
+        actions = ["s3:GetObject"]
+        effect = "Allow"
 
-    tags = {
-        "cloudfront" = "privateVod"
+        principals {
+            type = "AWS"
+            identifiers = [aws_cloudfront_origin_access_identity.private_vod_access_identity.iam_arn]
+        }
+
+        resources = [ "${aws_s3_bucket.vod_storage_bucket.arn}/*"]
+
+        sid = "cloudfront-private-access"
     }
 
-    viewer_certificate {
-        cloudfront_default_certificate = true
+    statement {
+        actions = ["s3:*"]
+        effect = "Deny"
+
+        principals {
+            type = "*"
+            identifiers = ["*"]
+        }
+
+        condition {
+            test = "Bool"
+            values = ["false"]
+            variable = "aws:SecureTransport"
+        }
+
+        resources = [ aws_s3_bucket.vod_storage_bucket.arn, "${aws_s3_bucket.vod_storage_bucket.arn}/*"]
+
+        sid = "enforce-ssl"
     }
 }
-*/
+
+resource "aws_s3_bucket_policy" "vod_bucket_policy" {
+    bucket = aws_s3_bucket.vod_storage_bucket.id
+    policy = data.aws_iam_policy_document.vod_access_policy.json
+}
 
 resource "aws_s3_bucket" "blob_storage_bucket" {
     bucket = "squadov-us-blob-bucket${var.bucket_suffix}"
@@ -120,5 +117,121 @@ resource "aws_s3_bucket" "blob_storage_bucket" {
 
     tags = {
         "s3" = "blobs"
+    }
+}
+
+resource "aws_cloudfront_public_key" "private_s3_vod_cloudfront_public_key" {
+    name = "private-s3-vod-cloudfront-public-key"
+    encoded_key = file("../../aws/keys/private_s3_vod_cloudfront_PUBLIC_KEY.pem")
+}
+
+resource "aws_cloudfront_key_group" "private_s3_vod_cloudfront_key_group" {
+    name = "private-s3-vod-cloudfront-key-group"
+    items = [ aws_cloudfront_public_key.private_s3_vod_cloudfront_public_key.id ]
+}
+
+resource "aws_cloudfront_distribution" "private_s3_vod_distribution" {
+    comment = "Private S3 VODs"
+
+    default_cache_behavior {
+        allowed_methods = ["GET", "HEAD", "OPTIONS"]
+        cached_methods = ["GET", "HEAD"]
+        compress = true
+        default_ttl = 86400
+
+        forwarded_values {
+            query_string = false
+
+            cookies {
+                forward = "none"
+            }
+        }
+
+        min_ttl = 3600
+        max_ttl = 604800
+        target_origin_id = "s3-bucket-origin"
+        trusted_key_groups = [
+            aws_cloudfront_key_group.private_s3_vod_cloudfront_key_group.id
+        ]
+
+        viewer_protocol_policy = "redirect-to-https"
+    }
+
+    enabled = true
+    is_ipv6_enabled = true
+    http_version = "http2"
+    price_class = "PriceClass_All"
+
+    tags = {
+        "vods" = "private_cloudfront"
+    }
+
+    restrictions {
+        geo_restriction {
+            restriction_type = "none"
+        }
+    }
+
+    viewer_certificate {
+        cloudfront_default_certificate = true
+    }
+
+    origin {
+        domain_name = aws_s3_bucket.vod_storage_bucket.bucket_domain_name
+        origin_id = "s3-bucket-origin"
+        s3_origin_config {
+            origin_access_identity = aws_cloudfront_origin_access_identity.private_vod_access_identity.cloudfront_access_identity_path
+        }
+    }
+}
+
+resource "aws_cloudfront_distribution" "public_s3_vod_distribution" {
+    comment = "Public S3 VODs"
+    
+    default_cache_behavior {
+        allowed_methods = ["GET", "HEAD", "OPTIONS"]
+        cached_methods = ["GET", "HEAD"]
+        compress = true
+        default_ttl = 86400
+
+        forwarded_values {
+            query_string = false
+
+            cookies {
+                forward = "none"
+            }
+        }
+
+        min_ttl = 3600
+        max_ttl = 604800
+        target_origin_id = "s3-bucket-origin"
+        viewer_protocol_policy = "redirect-to-https"
+    }
+
+    enabled = true
+    is_ipv6_enabled = true
+    http_version = "http2"
+    price_class = "PriceClass_All"
+
+    tags = {
+        "vods" = "public_cloudfront"
+    }
+
+    restrictions {
+        geo_restriction {
+            restriction_type = "none"
+        }
+    }
+
+    viewer_certificate {
+        cloudfront_default_certificate = true
+    }
+
+    origin {
+        domain_name = aws_s3_bucket.vod_storage_bucket.bucket_domain_name
+        origin_id = "s3-bucket-origin"
+        s3_origin_config {
+            origin_access_identity = aws_cloudfront_origin_access_identity.public_vod_access_identity.cloudfront_access_identity_path
+        }
     }
 }
