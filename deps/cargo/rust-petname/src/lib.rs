d08c5b7238b53c728671ef11589f48b360b9e6d6
Steven Tang <steven@squadov.gg>
2021-11-05 17:10:20 +0000

    add manual petnames dependency and remove some offensive words

---
+++ deps/cargo/rust-petname/src/lib.rs
@@ -0,0 +1,270 @@
+#![no_std]
+
+extern crate alloc;
+
+use alloc::{
+    string::{String, ToString},
+    vec::Vec,
+};
+
+use itertools::Itertools;
+use rand::seq::SliceRandom;
+
+/// Convenience function to generate a new petname from default word lists.
+#[allow(dead_code)]
+#[cfg(feature = "std_rng")]
+#[cfg(feature = "default_dictionary")]
+pub fn petname(words: u8, separator: &str) -> String {
+    Petnames::new().generate_one(words, separator)
+}
+
+/// A word list.
+pub type Words<'a> = Vec<&'a str>;
+
+/// Word lists and the logic to combine them into _petnames_.
+///
+/// A _petname_ with `n` words will contain, in order:
+///
+///   * `n - 2` adverbs when `n >= 2`, otherwise 0 adverbs.
+///   * 1 adjective when `n >= 2`, otherwise 0 adjectives.
+///   * 1 name / noun when `n >= 1`, otherwise 0 names.
+///
+#[derive(Clone, Debug, PartialEq)]
+pub struct Petnames<'a> {
+    pub adjectives: Words<'a>,
+    pub adverbs: Words<'a>,
+    pub names: Words<'a>,
+}
+
+impl<'a> Petnames<'a> {
+    /// Constructs a new `Petnames` from the default (small) word lists.
+    #[cfg(feature = "default_dictionary")]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Constructs a new `Petnames` from the small word lists.
+    #[cfg(feature = "default_dictionary")]
+    pub fn small() -> Self {
+        Self::init(
+            include_str!("../words/small/adjectives.txt"),
+            include_str!("../words/small/adverbs.txt"),
+            include_str!("../words/small/names.txt"),
+        )
+    }
+
+    /// Constructs a new `Petnames` from the medium word lists.
+    #[cfg(feature = "default_dictionary")]
+    pub fn medium() -> Self {
+        Self::init(
+            include_str!("../words/medium/adjectives.txt"),
+            include_str!("../words/medium/adverbs.txt"),
+            include_str!("../words/medium/names.txt"),
+        )
+    }
+
+    /// Constructs a new `Petnames` from the large word lists.
+    #[cfg(feature = "default_dictionary")]
+    pub fn large() -> Self {
+        Self::init(
+            include_str!("../words/large/adjectives.txt"),
+            include_str!("../words/large/adverbs.txt"),
+            include_str!("../words/large/names.txt"),
+        )
+    }
+
+    /// Constructs a new `Petnames` from the given word lists.
+    ///
+    /// The words are extracted from the given strings by splitting on whitespace.
+    pub fn init(adjectives: &'a str, adverbs: &'a str, names: &'a str) -> Self {
+        Self {
+            adjectives: adjectives.split_whitespace().collect(),
+            adverbs: adverbs.split_whitespace().collect(),
+            names: names.split_whitespace().collect(),
+        }
+    }
+
+    /// Keep words matching a predicate.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # #[cfg(feature = "default_dictionary")]
+    /// let mut petnames = petname::Petnames::default();
+    /// # #[cfg(feature = "default_dictionary")]
+    /// petnames.retain(|s| s.starts_with("b"));
+    /// # #[cfg(feature = "default_dictionary")]
+    /// # #[cfg(feature = "std_rng")]
+    /// petnames.generate_one(2, ".");
+    /// ```
+    ///
+    /// This is merely a convenience wrapper that applies the same predicate to
+    /// the adjectives, adverbs, and names lists.
+    ///
+    pub fn retain<F>(&mut self, mut predicate: F)
+    where
+        F: FnMut(&str) -> bool,
+    {
+        self.adjectives.retain(|word| predicate(word));
+        self.adverbs.retain(|word| predicate(word));
+        self.names.retain(|word| predicate(word));
+    }
+
+    /// Calculate the cardinality of this `Petnames`.
+    ///
+    /// If this is low, names may be repeated by the generator with a higher
+    /// frequency than your use-case may allow. If it is 0 (zero) the generator
+    /// will panic (unless `words` is also zero).
+    ///
+    /// This can saturate. If the total possible combinations of words exceeds
+    /// `u128::MAX` then this will return `u128::MAX`.
+    pub fn cardinality(&self, words: u8) -> u128 {
+        Lists(self, words)
+            .map(|list| list.len() as u128)
+            .fold1(u128::saturating_mul)
+            .unwrap_or(0u128)
+    }
+
+    /// Generate a new petname.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # #[cfg(all(feature = "std_rng", feature = "default_dictionary"))]
+    /// let mut rng = rand::thread_rng();
+    /// # #[cfg(all(feature = "std_rng", feature = "default_dictionary"))]
+    /// petname::Petnames::default().generate(&mut rng, 7, ":");
+    /// ```
+    ///
+    /// # Notes
+    ///
+    /// This may return fewer words than you request if one or more of the word
+    /// lists are empty. For example, if there are no adverbs, requesting 3 or
+    /// more words may still yield only "doubtful-salmon".
+    ///
+    pub fn generate<RNG>(&self, rng: &mut RNG, words: u8, separator: &str) -> String
+    where
+        RNG: rand::Rng,
+    {
+        Lists(self, words)
+            .filter_map(|list| list.choose(rng))
+            .cloned()
+            .intersperse(separator)
+            .collect::<String>()
+    }
+
+    /// Generate a single new petname.
+    ///
+    /// This is like `generate` but uses `rand::thread_rng` as the random
+    /// source. For efficiency use `generate` when creating multiple names, or
+    /// when you want to use a custom source of randomness.
+    #[cfg(feature = "std_rng")]
+    pub fn generate_one(&self, words: u8, separator: &str) -> String {
+        self.generate(&mut rand::thread_rng(), words, separator)
+    }
+
+    /// Iterator yielding petnames.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # #[cfg(all(feature = "std_rng", feature = "default_dictionary"))]
+    /// let mut rng = rand::thread_rng();
+    /// # #[cfg(all(feature = "std_rng", feature = "default_dictionary"))]
+    /// let petnames = petname::Petnames::default();
+    /// # #[cfg(all(feature = "std_rng", feature = "default_dictionary"))]
+    /// let mut iter = petnames.iter(&mut rng, 4, "_");
+    /// # #[cfg(all(feature = "std_rng", feature = "default_dictionary"))]
+    /// println!("name: {}", iter.next().unwrap());
+    /// ```
+    ///
+    pub fn iter<RNG>(&self, rng: &'a mut RNG, words: u8, separator: &str) -> Names<RNG>
+    where
+        RNG: rand::Rng,
+    {
+        Names {
+            petnames: self,
+            rng,
+            words,
+            separator: separator.to_string(),
+        }
+    }
+}
+
+#[cfg(feature = "default_dictionary")]
+impl<'a> Default for Petnames<'a> {
+    fn default() -> Self {
+        Self::small()
+    }
+}
+
+/// Iterator over a `Petnames`' word lists.
+///
+/// This yields the appropriate lists from which to select a word when
+/// constructing a petname of `n` words. For example, if you want 3 words in
+/// your petname, this will first yield the adverbs word list, then adjectives,
+/// then names.
+struct Lists<'a>(&'a Petnames<'a>, u8);
+
+impl<'a> Iterator for Lists<'a> {
+    type Item = &'a Words<'a>;
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, Some(self.1 as usize))
+    }
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let Self(petnames, ref mut word) = self;
+        match word {
+            0 => None,
+            1 => {
+                *word -= 1;
+                Some(&petnames.names)
+            }
+            2 => {
+                *word -= 1;
+                Some(&petnames.adjectives)
+            }
+            _ => {
+                *word -= 1;
+                Some(&petnames.adverbs)
+            }
+        }
+    }
+}
+
+/// Iterator yielding petnames.
+pub struct Names<'a, RNG>
+where
+    RNG: rand::Rng,
+{
+    petnames: &'a Petnames<'a>,
+    rng: &'a mut RNG,
+    words: u8,
+    separator: String,
+}
+
+impl<'a, RNG> Names<'a, RNG>
+where
+    RNG: rand::Rng,
+{
+    /// Calculate the cardinality of this iterator; see `Petnames::cardinality`.
+    #[allow(dead_code)]
+    pub fn cardinality(&self) -> u128 {
+        self.petnames.cardinality(self.words)
+    }
+}
+
+impl<'a, RNG> Iterator for Names<'a, RNG>
+where
+    RNG: rand::Rng,
+{
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(
+            self.petnames
+                .generate(self.rng, self.words, &self.separator),
+        )
+    }
+}
---
+++ deps/cargo/rust-petname/src/main.rs
@@ -0,0 +1,283 @@
+#[macro_use]
+extern crate clap;
+
+use petname::Petnames;
+
+use std::collections::HashSet;
+use std::fmt;
+use std::fs;
+use std::io::{self, Write};
+use std::path;
+use std::process;
+use std::str::FromStr;
+
+use clap::Arg;
+use rand::seq::IteratorRandom;
+
+fn main() {
+    let matches = app().get_matches();
+    match run(matches) {
+        Err(Error::Disconnected) => {
+            process::exit(0);
+        }
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            process::exit(1);
+        }
+        Ok(()) => {
+            process::exit(0);
+        }
+    }
+}
+
+enum Error {
+    Io(io::Error),
+    FileIo(path::PathBuf, io::Error),
+    Cardinality(String),
+    Alliteration(String),
+    Disconnected,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Error::Io(ref e) => write!(f, "{}", e),
+            Error::FileIo(ref path, ref e) => write!(f, "{}: {}", e, path.display()),
+            Error::Cardinality(ref message) => write!(f, "cardinality is zero: {}", message),
+            Error::Alliteration(ref message) => write!(f, "cannot alliterate: {}", message),
+            Error::Disconnected => write!(f, "caller disconnected / stopped reading"),
+        }
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(error: io::Error) -> Self {
+        Error::Io(error)
+    }
+}
+
+fn app<'a, 'b>() -> clap::App<'a, 'b> {
+    clap::App::new("rust-petname")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("Generate human readable random names.")
+        .after_help(concat!(
+            "Based on Dustin Kirkland's petname project ",
+            "<https://github.com/dustinkirkland/petname>."
+        ))
+        .arg(
+            Arg::with_name("words")
+                .short("w")
+                .long("words")
+                .value_name("WORDS")
+                .default_value("2")
+                .help("Number of words in name")
+                .takes_value(true)
+                .validator(can_be_parsed::<u8>),
+        )
+        .arg(
+            Arg::with_name("separator")
+                .short("s")
+                .long("separator")
+                .value_name("SEP")
+                .default_value("-")
+                .help("Separator between words")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("complexity")
+                .short("c")
+                .long("complexity")
+                .value_name("COM")
+                .possible_values(&["0", "1", "2"])
+                .hide_possible_values(true)
+                .default_value("0")
+                .help("Use small words (0), medium words (1), or large words (2)")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("directory")
+                .short("d")
+                .long("dir")
+                .value_name("DIR")
+                .help("Directory containing adjectives.txt, adverbs.txt, names.txt")
+                .conflicts_with("complexity")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("count")
+                .long("count")
+                .value_name("COUNT")
+                .default_value("1")
+                .help("Generate multiple names; pass 0 to produce infinite names!")
+                .takes_value(true)
+                .validator(can_be_parsed::<usize>),
+        )
+        .arg(
+            Arg::with_name("letters")
+                .short("l")
+                .long("letters")
+                .value_name("LETTERS")
+                .default_value("0")
+                .help("Maxiumum number of letters in each word; 0 for unlimited")
+                .takes_value(true)
+                .validator(can_be_parsed::<usize>),
+        )
+        .arg(
+            Arg::with_name("alliterate")
+                .short("a")
+                .long("alliterate")
+                .help("Generate names where each word begins with the same letter")
+                .takes_value(false),
+        )
+        .arg(
+            // For compatibility with upstream.
+            Arg::with_name("ubuntu")
+                .short("u")
+                .long("ubuntu")
+                .help("Alias; see --alliterate")
+                .takes_value(false),
+        )
+}
+
+fn run(matches: clap::ArgMatches) -> Result<(), Error> {
+    // Unwrapping is safe because these options have defaults.
+    let opt_separator = matches.value_of("separator").unwrap();
+    let opt_words = matches.value_of("words").unwrap();
+    let opt_complexity = matches.value_of("complexity").unwrap();
+    let opt_count = matches.value_of("count").unwrap();
+    let opt_letters = matches.value_of("letters").unwrap();
+    let opt_alliterate = matches.is_present("alliterate") || matches.is_present("ubuntu");
+
+    // Optional arguments without defaults.
+    let opt_directory = matches.value_of("directory");
+
+    // Parse numbers. Validated so unwrapping is okay.
+    let opt_words: u8 = opt_words.parse().unwrap();
+    let opt_count: usize = opt_count.parse().unwrap();
+    let opt_letters: usize = opt_letters.parse().unwrap();
+
+    // Load custom word lists, if specified.
+    let words = match opt_directory {
+        Some(dirname) => Words::load(dirname)?,
+        None => Words::Builtin,
+    };
+
+    // Select the appropriate word list.
+    let mut petnames = match words {
+        Words::Custom(ref adjectives, ref adverbs, ref names) => {
+            Petnames::init(&adjectives, &adverbs, &names)
+        }
+        Words::Builtin => match opt_complexity {
+            "0" => Petnames::small(),
+            "1" => Petnames::medium(),
+            "2" => Petnames::large(),
+            _ => Petnames::small(),
+        },
+    };
+
+    // If requested, limit the number of letters.
+    if opt_letters != 0 {
+        petnames.retain(|s| s.len() <= opt_letters);
+    }
+
+    // Check cardinality.
+    if petnames.cardinality(opt_words) == 0 {
+        return Err(Error::Cardinality(
+            "no petnames to choose from; try relaxing constraints".to_string(),
+        ));
+    }
+
+    // We're going to need a source of randomness.
+    let mut rng = rand::thread_rng();
+
+    // If requested, choose a random letter then discard all words that do not
+    // begin with that letter.
+    if opt_alliterate {
+        // We choose the first letter from the intersection of the first letters
+        // of each word list in `petnames`.
+        let firsts =
+            common_first_letters(&petnames.adjectives, &[&petnames.adverbs, &petnames.names]);
+        // Choose the first letter at random; fails if there are no letters.
+        match firsts.iter().choose(&mut rng) {
+            Some(c) => petnames.retain(|s| s.starts_with(*c)),
+            None => {
+                return Err(Error::Alliteration(
+                    "word lists have no initial letters in common".to_string(),
+                ))
+            }
+        };
+    }
+
+    // Get an iterator for the names we want to print out.
+    let names = petnames.iter(&mut rng, opt_words, opt_separator);
+
+    // Manage stdout.
+    let stdout = io::stdout();
+    let mut writer = io::BufWriter::new(stdout.lock());
+
+    // Stream if count is 0.
+    if opt_count == 0 {
+        for name in names {
+            writeln!(writer, "{}", name).map_err(suppress_disconnect)?;
+        }
+    } else {
+        for name in names.take(opt_count) {
+            writeln!(writer, "{}", name)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn can_be_parsed<INTO>(value: String) -> Result<(), String>
+where
+    INTO: FromStr,
+    <INTO as FromStr>::Err: std::fmt::Display,
+{
+    match value.parse::<INTO>() {
+        Err(e) => Err(format!("{}", e)),
+        Ok(_) => Ok(()),
+    }
+}
+
+fn common_first_letters(init: &[&str], more: &[&[&str]]) -> HashSet<char> {
+    let mut firsts = first_letters(init);
+    let firsts_other: Vec<HashSet<char>> = more.iter().map(|list| first_letters(list)).collect();
+    firsts.retain(|c| firsts_other.iter().all(|fs| fs.contains(c)));
+    firsts
+}
+
+fn first_letters(names: &[&str]) -> HashSet<char> {
+    names.iter().filter_map(|s| s.chars().next()).collect()
+}
+
+enum Words {
+    Custom(String, String, String),
+    Builtin,
+}
+
+impl Words {
+    // Load word lists from the given directory. This function expects to find three
+    // files in that directory: `adjectives.txt`, `adverbs.txt`, and `names.txt`.
+    // Each should be valid UTF-8, and contain words separated by whitespace.
+    fn load<T: AsRef<path::Path>>(dirname: T) -> Result<Self, Error> {
+        let dirname = dirname.as_ref();
+        Ok(Self::Custom(
+            read_file_to_string(dirname.join("adjectives.txt"))?,
+            read_file_to_string(dirname.join("adverbs.txt"))?,
+            read_file_to_string(dirname.join("names.txt"))?,
+        ))
+    }
+}
+
+fn read_file_to_string<P: AsRef<path::Path>>(path: P) -> Result<String, Error> {
+    fs::read_to_string(&path).map_err(|error| Error::FileIo(path.as_ref().to_path_buf(), error))
+}
+
+fn suppress_disconnect(err: io::Error) -> Error {
+    match err.kind() {
+        io::ErrorKind::BrokenPipe => Error::Disconnected,
+        _ => err.into(),
+    }
+}
